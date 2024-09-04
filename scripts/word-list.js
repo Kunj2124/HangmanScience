f1ddc24805245905e1684f993b9368df8df18cf0
Kunj <kunj.agarwal.243@gmail.com>
2024-09-04 12:03:08 +0530

    first commit

---
+++ scripts/word-list.js
@@ -0,0 +1,32 @@
+const wordList = [
+    { word: "astronomer", 
+        hint: "A person who studies celestial bodies and the universe." 
+    },
+    { word: "chemist", 
+        hint: "A professional who works in the field of chemical engineering." 
+    },
+    { word: "programmer", 
+        hint: "A specialist who develops software applications or systems." 
+    },
+    { word: "engineer", 
+        hint: "A person who works with electronics and electrical systems." 
+    },
+    { word: "geologist", 
+        hint: "A professional who studies the Earth's structure, history, and processes." 
+    },
+    { word: "architect", 
+        hint: "A professional who designs and constructs buildings and other structures." 
+    },
+    { word: "biologist", 
+        hint: "A scientist focused on the study of organisms and their environments." 
+    },
+    { word: "psychiatrist", 
+        hint: "A healthcare professional specializing in diagnosing and treating mental disorders." 
+    },
+    { word: "cardiologist", 
+        hint: "A medical specialist focused on treating heart diseases." 
+    },
+    { word: "mechanic", 
+        hint: "A person who fixes and builds machines." 
+    },
+];
